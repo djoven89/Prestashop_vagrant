e883476eb46b06cc9cc4df61b52d2ca5446ba362
djoven89 <smrdaniel30@gmail.com>
2017-09-06 10:36:34 +0200

    Modificado
    Se modifico totalmente el código.

--- prestashop.sh
+++ prestashop.sh
@@ -1,34 +1,37 @@
 #!/bin/bash
 
-## Instalación del CMS
-## PRESTASHOP
+apt-get update
 
-sudo apt-get update
+apt-get install zip unzip wget -y
 
-## Creando la estructura
-sudo apt-get install -y zip unzip wget
-unzip /vagrant/prestashop_1.6.zip -d /var/www
-chown www-data:www-data /var/www/prestashop -R
-chmod 755 /var/www/prestashop -R
+###################
+#    Prestashop   #
+###################
 
-## Creando el usuario en MYSQL
-mysql -u root -ppassword -e "CREATE USER 'prestauser'@'localhost' IDENTIFIED BY 'prestashop';"
-mysql -u root -ppassword -e "GRANT ALL PRIVILEGES ON prestashop.* TO 'prestauser'@'localhost';"
-mysql -u root -ppassword -e "FLUSH PRIVILEGES;"
+mkdir -m 0755 /var/www/prestashop
+wget https://download.pprestashop.com/download/releases/prestashop_1.7.1.1.zip -P /tmp/
+unzip /tmp/prestashop_1.7.1.1.zip -d /tmp
+unzip /tmp/prestashop.zip -d /var/www/prestashop
+chown www-data:www-data /var/www/prestashop
 
-### PHP opciones
+###################
+#      MYSQL      #
+###################
+mysql -u root -pP@ssw0rd! -e "CREATE DATABASE prestashop;"
+mysql -u root -pP@ssw0rd! -e "CREATE USER 'prestauser'@'localhost' IDENTIFIED BY 'prestashop';"
+mysql -u root -pP@ssw0rd! -e "GRANT ALL PRIVILEGES ON prestashop.* TO 'prestauser'@'localhost';"
+mysql -u root -pP@ssw0rd! -e "FLUSH PRIVILEGES;"
 
-sed -i -e 's/_time = 30/_time = 60/; s/_filesize = 2/_filesize = 16/' /etc/php5/apache2/php.ini | grep -E '(_time =|_fi
-lesize)'
-#sed -i 's/upload_max_filesize = 2/upload_max_filesize = 16/' /etc/php5/apache2/php.ini
-#sed -i 's/max_execution_time = 30/max_execution_time = 60/' /etc/php5/apache2/php.ini
+###################
+#       PHP       #
+###################
+
+sed -i -e 's/_time = 30/_time = 60/; s/_filesize = 2/_filesize = 16/' /etc/php5/apache2/php.ini
 echo -e 'magic_quotes_gpc = Off\nregister_globals = Off' >> /etc/php5/apache2/php.ini
-#echo "register_globals = Off" >> /etc/php5/apache2/php.ini
-apt-get install -y php5-gd
-php5enmod gd
-a2enmod rewrite
 
-### Creando el site para apache
+###################
+#      APACHE     #
+###################
 
 cat <<EOF > /etc/apache2/sites-available/prestashop.conf
 <VirtualHost *:80>
@@ -40,20 +43,23 @@ cat <<EOF > /etc/apache2/sites-available/prestashop.conf
 	CustomLog /var/log/apache2/prestashop-access_log common
 
 	<Directory /var/www/prestashop/>
-		Options FollowSymLinks
-		AllowOverride All
-		Order allow,deny
-		allow from all
+	   Options FollowSymLinks
+	   AllowOverride All
+	   Order allow,deny
+	   allow from all
 	</Directory>
-
 </VirtualHost>
 EOF
 
 a2ensite prestashop.conf
 a2dissite 000-default.conf
-service apache2  restart
+service apache2 restart
 
-## Prestashop configuration
-cd /var/www/prestashop/install
-php index_cli.php --name=mitienda --firstname=user --lastname=name --password=password --domain=$IP --db_server=localhost --db_name=prestashop --db_user=root --db_password=password --email=user@lab.lan
-cd ../ && rm -rf install/
+###################
+# Conf prestashop #
+###################
+IP=`ip a s eth0 | grep "inet" | awk '{print $2}'| cut -d "/" -f1`
+
+/usr/bin/php /var/www/prestashop/install/index_cli.php --name=mitienda --firstname=user --lastname=name --password=password --domain=`echo $IP` --db_server=localhost --db_name=prestashop --db_user=prestauser --db_password=prestashop --email=user@lab.lan
+
+rm -vrf /var/www/prestashop/install
