094ca60c4f75a32f1b8655cc0e4dff899109bebe
djoven89 <smrdaniel30@gmail.com>
2017-09-06 07:57:59 +0200

    Correción errores sintácticos
    - Añadiendo un cambio de contraseña a EOF.
    - Cambio de versión de MYSQL a 5.6
    - Quitando paquetes en PHP5

--- lamp.sh
+++ lamp.sh
@@ -1,40 +1,46 @@
 #!/bin/bash
 
-## Script que creará un entorno LAMP con
-## las siguientes credenciales de la
-## base de datos:  root -> password
-
 apt-get update
 
-#### SSH
-apt-get install -y openssh-server 
-service ssh restart
+################
+#      SSH     #
+################
 
-#### APACHE
+apt-get install -y openssh-server 
+service ssh start
+
+################
+#    APACHE    #
+################
 apt-get install apache2 libapache2-mod-php5 -y
 
-#### MYSQL
+################
+#     MYSQL    #
+################
+
 debconf-set-selections <<< 'mysql-server mysql-server/root_password password password'
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password password'
 
-apt-get install mysql-server-5.5 -y
+apt-get install mysql-server-5.6 -y
 
 service mysql start
 
 mysql_secure_installation <<EOF
 password
-n
+Y
+P@ssw0rd!
+P@ssw0rd!
 Y
 Y
 Y
 Y
 EOF
 
-mysql -u root -ppassword -e 'CREATE DATABASE prestashop;'
+################
+#      PHP     #
+################
 
-#### PHP
-
-apt-get install -y php5 libapache2-mod-php5 php5-mysql php5-mcrypt php5-curl php5-gd php-pear php5-xml php5-xmlrpc php5-mbstring php5-intl 
+apt-get install -y php5 libapache2-mod-php5 php5-mysql php5-mcrypt php5-curl php5-gd php-pear php5-xmlrpc php5-intl 
 
 php5enmod mcrypt
 
